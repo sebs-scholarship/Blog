0f547c91682de4fa737ecd0eefa458876459023d
Levi Muniz <levi.muniz17@gmail.com>
2021-11-22 19:59:39 -0700

    Fixed incorrect usage of powershell syntax in batch script

--- scripts/serve_local.bat
+++ scripts/serve_local.bat
@@ -1,2 +1,2 @@
-docker run -v ${PWD}:/srv/jekyll -v ${PWD}\_site:/srv/jekyll/_site -p 4000:4000 --rm \
+docker run -v %cd%:/srv/jekyll -v %cd%\_site:/srv/jekyll/_site -p 4000:4000 --rm \
 jekyll/builder:4.1.0 /bin/bash -c "chmod 777 /srv/jekyll && jekyll serve --future --drafts"
