33b6e1e8745baa107f81c54d088be4ea4341f19a
Levi Muniz <levi.muniz17@gmail.com>
2022-12-01 14:54:47 -0700

    Bump jekyll/builder image from 4.1.0 to 4.2.2

--- scripts/serve_local.bat
+++ scripts/serve_local.bat
@@ -1,2 +1,2 @@
 docker run -v %cd%:/srv/jekyll -v %cd%\_site:/srv/jekyll/_site -p 4000:4000 --rm \
-jekyll/builder:4.1.0 /bin/bash -c "chmod 777 /srv/jekyll && jekyll serve --future --drafts"
+jekyll/builder:4.2.2 /bin/bash -c "chmod 777 /srv/jekyll && jekyll serve --future --drafts"
