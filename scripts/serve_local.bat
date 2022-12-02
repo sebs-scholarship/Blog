51250f12d8931623d3cb2044e6ae98589ee6138d
Levi Muniz <levi.muniz17@gmail.com>
2022-12-01 18:09:03 -0700

    Remove unnecessary docker volume mount

--- scripts/serve_local.bat
+++ scripts/serve_local.bat
@@ -1,2 +1,2 @@
-docker run -v %cd%:/srv/jekyll -v %cd%\_site:/srv/jekyll/_site -p 4000:4000 --rm \
+docker run -v %cd%:/srv/jekyll -p 4000:4000 --rm ^
 jekyll/builder:4.2.2 /bin/bash -c "chmod 777 /srv/jekyll && jekyll serve --future --drafts"
