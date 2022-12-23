fd24316e602332c4c1f0a70b2ffa1566eb61bdfd
Levi Muniz <levi.muniz17@gmail.com>
2022-12-22 17:02:10 -0700

    Install and configure image gen plugin

--- scripts/serve_local.bat
+++ scripts/serve_local.bat
@@ -1,2 +1,8 @@
 docker run -v %cd%:/srv/jekyll -p 4000:4000 --rm ^
-jekyll/builder:4.2.2 /bin/bash -c "chmod 777 /srv/jekyll && jekyll serve --future --drafts"
+jekyll/builder:4.2.2 /bin/bash -c "apk add --no-cache imagemagick wget ^
+&& wget 'https://fonts.google.com/download?family=Montserrat' -O font.zip ^
+&& unzip font.zip -d /usr/share/fonts/googlefonts/ ^
+&& rm font.zip ^
+&& chmod 755 /srv/jekyll ^
+&& chmod 755 /srv/jekyll/assets/images ^
+&& jekyll serve --future --drafts"
