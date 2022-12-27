5fbee2b9488594c8d495f7e437a6794081f1d58c
Levi Muniz <levi.muniz17@gmail.com>
2022-12-27 01:40:08 -0700

    Change file permissions in CI, local dev, and forestry again

--- scripts/serve_local.bat
+++ scripts/serve_local.bat
@@ -3,6 +3,6 @@ jekyll/builder:4.2.2 /bin/bash -c "apk add --no-cache imagemagick wget ^
 && wget 'https://fonts.google.com/download?family=Montserrat' -O font.zip ^
 && unzip font.zip -d /usr/share/fonts/googlefonts/ ^
 && rm font.zip ^
-&& chmod 766 /srv/jekyll ^
-&& chmod 766 /srv/jekyll/assets/images ^
+&& chmod 777 /srv/jekyll ^
+&& chmod 777 /srv/jekyll/assets/images ^
 && jekyll serve --future --drafts"
