12ce33c72b6f29121120f4b708a0db69b59027fa
Levi Muniz <levi.muniz17@gmail.com>
2022-12-27 01:32:59 -0700

    Change file permissions in CI, local dev, and forestry

--- scripts/serve_local.sh
+++ scripts/serve_local.sh
@@ -5,6 +5,6 @@ docker run -v "$(pwd)":/srv/jekyll -p 4000:4000 --rm jekyll/builder:4.2.2 \
 && wget 'https://fonts.google.com/download?family=Montserrat' -O font.zip \
 && unzip font.zip -d /usr/share/fonts/googlefonts/ \
 && rm font.zip \
-&& chmod 755 /srv/jekyll \
-&& chmod 755 /srv/jekyll/assets/images \
+&& chmod 766 /srv/jekyll \
+&& chmod 766 /srv/jekyll/assets/images \
 && jekyll serve --future --drafts"
