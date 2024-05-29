4149b82125425e2098d647b56db9b3e3874d18b9
Levi Muniz <levi.muniz17@gmail.com>
2024-05-28 22:33:58 -0600

    Update workflow to use jpig image (#144)

--- Gemfile
+++ Gemfile
@@ -10,7 +10,7 @@ group :jekyll_plugins do
   gem "jekyll-algolia", ">= 1.7.1"
   gem "jekyll-feed", ">= 0.15.1"
   gem "jekyll-include-cache", ">= 0.2.1"
-  gem 'jekyll-post-image-generator', github: 'sebs-scholarship/jekyll-post-image-generator', tag: 'v0.0.9'
+  gem 'jekyll-post-image-generator', github: 'sebs-scholarship/jekyll-post-image-generator', tag: 'v0.0.10'
   gem "jekyll-relative-links", ">= 0.6.1"
   gem "jekyll-remote-theme", ">= 0.4.3"
   gem "jekyll-sitemap", ">= 1.4.0"
--- scripts/serve_local.bat
+++ scripts/serve_local.bat
@@ -1,8 +1,6 @@
-docker run -v %cd%:/srv/jekyll -p 4000:4000 --rm ^
-jekyll/builder:4.2.2 /bin/bash -c "apk add --no-cache imagemagick wget ^
-&& wget 'https://fonts.google.com/download?family=Montserrat' -O font.zip ^
-&& unzip font.zip -d /usr/share/fonts/googlefonts/ ^
-&& rm font.zip ^
-&& chmod 777 /srv/jekyll ^
+docker run -v %cd%:/srv/jekyll -p 4000:4000 -it --rm ^
+-e JEKYLL_UID=1001 -e JEKYLL_GID=1001 -e BUNDLE_FROZEN=true -e BUNDLE_PATH='_vendor/bundle' ^
+leviem1/jpig:0.0.10 /bin/bash -c ^
+"chmod 777 /srv/jekyll ^
 && chmod 777 /srv/jekyll/assets/images ^
-&& jekyll serve --future --drafts"
+&& bundle exec jekyll serve --future --drafts"
